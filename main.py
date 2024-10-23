78470c920015020dda5966119553737bfa2c2cae
kevinpedroso <kevinpedroso690@gmail.com>
2024-10-23 09:44:27 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Dilan")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Dilan y Kevin")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Kevin")
+})
